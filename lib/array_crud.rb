e2703b445b248e62e30f08aeb223da305444f95f
LeoBlaze87 <johnniewilliams87@gmail.com>
2020-11-17 22:20:02 -0500

    commit tree fd84b749798735e4607e13ad0398b168194404c6

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
+require "pry"
 def create_an_empty_array
-  
+    []
 end
 
 def create_an_array
-  
+  create_an_array = [" ", " ", " ", " "]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    Array(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  Array(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+ "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-  
+    "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  "am"
 end
 
 def retrieve_first_element_from_array(array)
-  
+  "wow"
 end
 
 def retrieve_last_element_from_array(array)
-  
+  "arrays!"
 end
